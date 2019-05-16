1fcd9392b09db393c7a6366f53348f1a5f86e135
Anna <annas.wieclaw@gmail.com>
2019-05-16 10:11:59 +0100

    Add before method to the test to set the notes

--- spec/my_notes_spec.rb
+++ spec/my_notes_spec.rb
@@ -4,15 +4,22 @@ describe "MyNotes" do
   let(:my_notes) {
     MyNotes.new
   }
-  it "can add a note with title and body" do
-    my_notes.add("Have Breakfast", "Breakfast will give you energy to start the day happily")
-    expect(my_notes.notes).to include(:title => "Have Breakfast", :body => "Breakfast will give you energy to start the day happily")
-  end
-
-  it "can list all the note titles" do
+  before(:each) do
     my_notes.add("Have Breakfast", "Breakfast will give you energy to start the day happily")
     my_notes.add("Exercise", "Yoga is good for mind and body")
     my_notes.add("Read a book", "Work on your imagination")
+  end
+  it "can add a note with title and body" do
+    my_notes.add("Blog", "Write me")
+    expect(my_notes.notes).to include(:title => "Blog", :body => "Write me")
+  end
+
+  it "can list all the note titles" do
     expect(my_notes.title_list).to eq(["Have Breakfast", "Exercise", "Read a book"])
+  end
+
+  it "allows user to pick a note and see its title and body" do
+    
+    #expect(my_notes.pick_me(1)).to eq({:title => "Have Breakfast", :body => "Breakfast will give you energy to start the day happily" })
   end
 end
